166f884996251cef76ed0d71b30db5048dd7ad6b
Jesse Pimenta <drrrreams@gmail.com>
2015-07-06 12:18:04 -0600

    made click function for start and stop game

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+var count = 30;
+
 $(function(){
+
   $('.dots').click(function(){
     console.log('works');
   });
+
+  $('.startStop').click(function(){
+    console.log('works');
+  });
+
+
+
+
+
 });
