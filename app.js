908ab7a541afa705fa8e10cebcd98f7ffe6b0ce8
Jesse Pimenta <drrrreams@gmail.com>
2015-07-07 09:25:48 -0600

    added score function

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ var countdownCurrent=0;
 var countdown=false;
 var countdownInterval;
 var countdownEnd = -1;
+var randomLight = Math.floor(Math.random() * 60);
+var score=0;
 
 $(function(){
 
 $('#begin').click(function(){
+
+  randomLight = Math.floor(Math.random() * 60);
+  console.log(randomLight);
+  $('.dots')[randomLight].focus(function(){
+    alert( "Handler for .focus() called." );
+  });
+  console.log($('.dots')[randomLight]);
+
     countdownCurrent = countdownMax;
     console.log(countdownCurrent+" / "+countdownMax);
     countdownInterval = setInterval(function(){
@@ -20,20 +30,26 @@ $('#begin').click(function(){
 
       }, 1000);
 
-
 });
 
-
   $('.dots').click(function(){
-    var randomLight = Math.floor(Math.random() * 60);
-    console.log(randomLight);
-    $('.dots')[randomLight].focus(function(){
+    if($(this).is(":focus")){score++}
+    console.log(score);
+  $('#score')[0].value = score;
+    var randomLight2 = Math.floor(Math.random() * 60);
+    console.log(randomLight2);
+    $('.dots')[randomLight2].focus(function(){
       alert( "Handler for .focus() called." );
     });
-    console.log($('.dots')[randomLight]);
+    console.log($('.dots')[randomLight2]);
   });
 
   $('.begin').click(function(){
+    var randomLight = Math.floor(Math.random() * 60);
+    console.log(randomLight);
+    $('.dots')[randomLight].focus(function(){
+    alert ("Handler for .focus() called.");
+    });
     console.log('works');
   });
 
