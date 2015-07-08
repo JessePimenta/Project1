98f122f5c807f0d46d3d8190af10625010f93c24
Jesse Pimenta <drrrreams@gmail.com>
2015-07-07 18:37:55 -0600

    created variables for 2pgame possibility

--- app.js
+++ app.js
@@ -6,13 +6,35 @@ var countdown=false;
 var countdownInterval;
 var countdownEnd = -1;
 var randomLight = Math.floor(Math.random() * 60);
-var score=0;
+var score= 0;
+var player1= 1;
+var player2= 2;
+var player1Points;
+var player2Points;
+var player1round1;
+var player1round2;
+var player1round3;
+var player2round1;
+var player2round2;
+var player2round3;
+var player1Wins;
+var player2Wins;
+var playerMove = 0
+
 
 $(function(){
 
 $('#begin').click(function(){                          // starts game
 
-  randomLight = Math.floor(Math.random() * 60);       // i
+  if (counter % 2===0 ){
+  	$(this).text('o');
+  }
+  else {
+  	$(this).text('x');
+  }
+
+
+  randomLight = Math.floor(Math.random() * 60);       //
   console.log(randomLight);
   $('.dots')[randomLight].focus(function(){
     alert( "Handler for .focus() called." );
@@ -25,6 +47,9 @@ $('#begin').click(function(){                          // starts game
       $('#countdownHolder')[0].value = countdownCurrent--;
       if(countdownCurrent === countdownEnd){
         clearTimeout(countdownInterval);
+
+
+
         alert('game over!');
       }
 
