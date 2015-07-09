b4a5fa2a4eca0e3122f519f215e154b8f37e940c
Jesse Pimenta <drrrreams@gmail.com>
2015-07-09 15:12:48 -0600

    added volume control slider

--- app.js
+++ app.js
@@ -51,6 +51,21 @@ $(function(){
 
   });
 
+  $('#audioSlider').slider({
+    orientation: "vertical",
+    value: audio1.volume,
+    min: 0,
+    max: 1,
+    range: 'min',
+    animate: true,
+    step: 0.1,
+    slide: function(e, ui) {
+        audio1.volume = ui.value;
+    }
+});
+
+
+
 
 $('#begin').click(function(){                          // starts game
 
